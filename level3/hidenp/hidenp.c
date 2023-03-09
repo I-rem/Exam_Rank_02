f58968b33ce595974e8a4e23968acab06be06f7f
IremK <ayseirem.kayacioglu@gmail.com>
2023-03-09 13:43:36 +0300

    add hidenp, print_hex

--- level3/hidenp/hidenp.c
+++ level3/hidenp/hidenp.c
@@ -4,8 +4,21 @@ int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-
+		while (*argv[1] != '\0' && *argv[2] != '\0')
+		{
+			if (*argv[1] == *argv[2])
+			{
+				argv[1]++;
+				argv[2]++;
+			}
+			else
+				argv[2]++;
+		}
+		if (*argv[1] == '\0')
+			write(1, "1", 1);
+		else
+			write(1, "0", 1);
 	}
-	write(1, "0\n", 2);
+	write(1, "\n", 1);
 	return (0);
 }
--- level3/print_hex/print_hex.c
+++ level3/print_hex/print_hex.c
@@ -1,9 +1,28 @@
 #include <unistd.h>
 
+void puthex(int num)
+{
+	char hex[16] = "0123456789abcdef";
+	if (num < 16)
+		write(1, &hex[num], 1);		
+	else 
+	{
+		puthex(num / 16);
+		puthex (num % 16);	
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
+		int num = 0;
+		while (*argv[1])
+		{
+			num = num * 10 + *argv[1] - '0';
+			argv[1]++;
+		}
+		puthex(num);
 	}
 	write(1, "\n", 1);
 	return (0);
