71709d60a7e9b77a91ea1d639a3ad22eba63e29a
IremK <ayseirem.kayacioglu@gmail.com>
2023-03-09 15:15:05 +0300

    Add rstr_capitalizer and fprime

--- level3/rstr_capitalizer/rstr_capitalizer.c
+++ level3/rstr_capitalizer/rstr_capitalizer.c
@@ -4,7 +4,35 @@ int main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
+		int i = 0;
+		int j = 1;
+		while (argv[j])
+		{
+			int len = 0;
+			int i = 0;
+			while (argv[j][len])
+				len++;
+			len--;
+			while (argv[j][len] == ' ' || argv[j][len] == '\t')
+				len--;
+			while (argv[j][i])
+			{
+				if (argv[j][i + 1] == ' ' || argv[j][i + 1] == '\t' || argv[j][i + 1] == '\0')
+				{
+					if (argv[j][i] >= 'a' && argv[j][i] <= 'z')
+					argv[j][i] -= 32;
+				}
+				else
+				{
+					if (argv[j][i] >= 'A' && argv[j][i] <= 'Z')
+						argv[j][i] += 32;
+				}
+				write(1, &argv[j][i], 1);
+				i++;
+			}
+			write(1, "\n", 1);
+			j++;
+		}
 	}
-	write(1, "\n", 1);
 	return (0);
 }
--- level4/fprime/fprime.c
+++ level4/fprime/fprime.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_prime(int num)
+{
+	int i = 2;
+	while (i <= num / 2)
+	{
+		if (num % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 int main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-
+		int i = 2;
+		int num = 0;
+		num = atoi(argv[1]);
+		if (num == 1)
+			printf("1\n");
+		else
+		{
+			while (i <= num)
+			{
+				if (is_prime(i) && num % i == 0)
+				{
+					printf("%d", i);
+					num /= i;
+					if (num != 1)
+						printf("*");
+					i--;
+				}
+				i++;
+			}
+			printf("\n");
+		}
 	}
-	write(1, "\n", 1);
+	else
+		write(1, "\n", 1);
 }
