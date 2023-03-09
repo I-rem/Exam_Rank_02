fc50e95b2f1c665063c45d6ace36284408fec9e5
IremK <ayseirem.kayacioglu@gmail.com>
2023-03-09 14:24:59 +0300

    Add lcm and ft_split

--- level3/lcm/lcm.c
+++ level3/lcm/lcm.c
@@ -1,4 +1,17 @@
 unsigned int    lcm(unsigned int a, unsigned int b)
 {
-
+	if (a == 0 || b == 0)
+		return (0);
+	else
+	{
+		int hcf = 1;
+		int i = 2;
+		while (i <= a && i <= b)
+		{
+			if (a % 2 == 0 && b % 2 == 0)
+				hcf = i;
+			i++;
+		}
+		return (abs(a * b) / hcf);
+	}	
 }
--- level4/ft_split/ft_split.c
+++ level4/ft_split/ft_split.c
@@ -1,6 +1,40 @@
 #include <stdlib.h>
 char    **ft_split(char *str)
 {
-
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		str++;
+	int len = 0;
+	while(str[len])
+		len++;
+	len--;
+	while(str[len] == ' ' || str[len] == 't' || str[len] == '\n')
+		len--;
+	str[len + 1] = '0';
+	
+	int wordCount = 1;
+	int i = 0;
+	while(str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\t')
+			wordCount++;
+		while (str[i] == ' ' || str[i] == '\t')
+			i++;
+		i++;
+	}
+	char **words = (char **)malloc(sizeof(char *) * (wordCount + 1));
+	i = 0;
+	int j = 0;
+	while (*str)
+	{
+		j = 0;
+		while(*str != ' ' || *str != '\t' || *str != '0')
+		{
+			words[i][j] = *str;
+			str++;
+			j++;
+		}
+		i++;
+	}
+	return (words);
 }
 
